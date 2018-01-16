dc3e117b59230b4d932004f7fe8fec2ce42f633c
Balázs Indig <indba@digitus.itk.ppke.hu>
2018-01-16 14:48:10 +0100

    Filter bad index entries

--- filter_index.sh
+++ filter_index.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
 
+# 1) Filter URL-s with robots.txt at the end
+# 2) Keep entries with status 200 only
+# 3) Normalize mime-type field
+# 4) Prefix domain (sed)
+# 5) Keep only the allowed mime-types (fgrep)
+# 6) Filter entries permanently resulting in decompression errors (bad index?) (fgrep)
+# 7) Sort by the domain
+
+
+filter_file() {
 if command -v mawdfk >/dev/null; then
     AWK=mawk
 else
     AWK=awk
 fi
+echo $1
+echo $2
 mkdir -p $2 && \
 gzip -cd $1 | ${AWK} '{if ($1 !~ /\/robots.txt/ && $5 == "200") {
                            gsub(/\\?\"/,"", $6)
@@ -13,4 +26,10 @@ gzip -cd $1 | ${AWK} '{if ($1 !~ /\/robots.txt/ && $5 == "200") {
                            print $1,$2,$3,$4,$5,$6
                       }
                      }' | sed -r 's#(^https?://((www|ww2|ww3|www2|www3)[^ ])?([^/ ]+)(:[0-9]+)?/?[^ ]*)#\4 \1#' | \
-                     fgrep -wf allowed_mimes.txt | LC_ALL=C sort -t ' ' -k1,1 | gzip > $2/`basename $1`
+                     fgrep -wf allowed_mimes.txt | fgrep -xvf bad_index.txt | LC_ALL=C sort -t ' ' -k1,1 | \
+                     gzip > $2/`basename $1`
+}
+
+for file in `ls $1`; do
+    filter_file "$1/${file}" $2
+done
