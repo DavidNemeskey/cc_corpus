dd2c29cfef3a51550ff9ce76a79bb6a1c89727a0
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-01-02 21:42:31 +0100

    Progres...

--- remove_boilerplate.py
+++ remove_boilerplate.py
@@ -24,7 +24,7 @@ class IndexWarcReader:
     downloaded WARC segments from Common Crawl". One difference to a CC WARC
     file is that these files only contain the responses.
     """
-    def __init__(self, index_dir, warc_dir):
+    def __init__(self, index_dir, warc_dir, output_dir):
         """
         Creates a new IndexWarcReader with the specified index and warc
         directories. These must be compatible, i.e. the WARC directory should
@@ -36,11 +36,15 @@ class IndexWarcReader:
         """
         self.index_dir = index_dir
         self.warc_dir = warc_dir
+        self.output_dir = output_dir
+        # This is the output stream
+        self.outf = None
 
-    def read(self, index_file):
+    def read(self, index_file, func):
         """
         Enumerates the index and WARC records in the specified index file and
-        the matching WARC files.
+        the matching WARC files. Calls the specified function with the two
+        records.
         """
         index_iter = self.index_lines(index_file)
         warc_iter = self.warc_records(index_file)
@@ -48,10 +52,22 @@ class IndexWarcReader:
             url = warc_record['WARC-Target-URI']
             for index in index_iter:
                 if index.url == url:
-                    print(url)
+                    self.process_record(index, warc_record)
                     break
             else:
                 raise ValueError('URL {} was not found in index'.format(url))
+
+    def process_record(self, index, warc):
+        """Writes the output file."""
+        print('<doc domain="{0}" index="{1}" url="{2}" warc-file="{3}" ' \
+              'offset="{4}" length="{5}" response="{6}" mime-type="{7}">\n' \
+              '<meta>\n<request>\n{8}\n</request>\n<response>\n{9}\n'
+              '</response>\n</meta>\n{10}\n</doc>\n\n\n'.format(
+                  index.domain, index.index, index.url, index.warc_file,
+                  index.offset, index.length, index.status, index.mime,
+                  # warc1, warc2, text_removed).encode('UTF-8'),
+                  'a', 'b', 'c').encode('UTF-8'),
+              file=self.outf)
 
     def index_lines(self, index_file):
         """Enumerates the lines of the index file into IndexTuples."""
@@ -66,9 +82,15 @@ class IndexWarcReader:
         Enumerates WARC records from the WARC files that correspond to
         index_file.
         """
-        for warc_file in self.warc_files_for_index(index_file):
-            for record in warc.open(warc_file):
-                yield record
+        try:
+            for warc_file in self.warc_files_for_index(index_file):
+                output_file = op.basename(warc_file).replace('.warc.', '.txt.')
+                with gzip.open(op.join(self.output_dir, output_file), 'wt') as outf:
+                    self.outf = outf
+                    for record in warc.open(warc_file):
+                        yield record
+        finally:
+            self.outf = None
 
     def warc_files_for_index(self, index_file):
         """Returns all WARC files that correspond to an index file."""
