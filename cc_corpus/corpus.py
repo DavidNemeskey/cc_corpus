9003db82b5c37ca157b826948bd572f1a4cc263b
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-01-17 13:31:19 +0100

    Parsing works perfectly.

--- cc_corpus/corpus.py
+++ cc_corpus/corpus.py
@@ -4,8 +4,12 @@
 """Contains code that works with the semi-XML format of the corpus."""
 
 from collections import OrderedDict
+import concurrent.futures as cf
 import io
+from queue import Empty, Queue
 import re
+import sys
+import threading
 
 from cc_corpus.utils import openall
 
@@ -18,10 +22,10 @@ class ParseError(Exception):
 class Document:
     uri_p = re.compile('^WARC-Target-URI: (.+?)$', re.M)
 
-    def __init__(self, attrs=True, meta=True, content=True):
-        self.attrs = OrderedDict() if attrs else None
-        self.meta = OrderedDict() if meta else None
-        self.paragraphs = [] if content else None
+    def __init__(self, attrs=None, meta=None, paragraphs=None):
+        self.attrs = attrs
+        self.meta = meta
+        self.paragraphs = paragraphs
 
     def content(self):
         """
@@ -55,7 +59,7 @@ class Document:
         if self.paragraphs:
             for p in self.paragraphs[:-1]:
                 print('<p>\n{}\n</p>\n'.format(p), file=buffer)
-            print('<p>\n{}\n</p>'.format(p), file=buffer)
+            print('<p>\n{}\n</p>'.format(self.paragraphs[-1]), file=buffer)
         print('</doc>', file=buffer)
         return buffer.getvalue()
 
@@ -79,8 +83,8 @@ class Document:
 
 class SAXParser:
     """A SAX-like parser for the corpus format."""
-    tag_p = re.compile(r'^<([^\s>]+)(?:\s+\S+="[^"]*")*\s*>$')
-    attrs_p = re.compile(r'(\S)+="([^"]*)"')
+    tag_p = re.compile(r'^<([^\s>]+)((?:\s+\S+="[^"]*")*)\s*>$')
+    attrs_p = re.compile(r'(\S+)="([^"]*)"')
 
     def __init__(self, handler, attrs=True):
         """
@@ -101,30 +105,37 @@ class SAXParser:
         and is used for debugging.
         """
         stack = []
+        fn_msg = 'in file {} '.format(filename) if filename else ''
         for line_no, line in enumerate(map(str.strip, corpus_stream), start=1):
-            m = SAXParser.tag_p.match(line)
-            if m:
-                tag = m.group(1)
-                if not tag.startswith('/'):
-                    stack.append(tag)
-                    if self.attrs:
-                        attrs = OrderedDict()
+            try:
+                m = SAXParser.tag_p.match(line)
+                if m:
+                    tag, attrs = m.groups()
+                    if not tag.startswith('/'):
+                        stack.append(tag)
+                        if self.attrs and attrs:
+                            attrs = OrderedDict(m.groups() for m in
+                                                SAXParser.attrs_p.finditer(attrs))
+                        else:
+                            attrs = None
+                        self.handler.startElement(tag, attrs)
                     else:
-                        attrs = None
-                    self.handler.startElement(tag, attrs)
+                        if not stack or stack.pop() != tag[1:]:
+                            msg = 'Closed unpaired tag {} {}on line {}'.format(
+                                tag[1:], fn_msg, line_no)
+                            raise ParseError(msg)
+                        self.handler.endElement(tag[1:])
                 else:
-                    if not stack or stack.pop() != tag[1:]:
-                        fn = 'in file {} '.format(filename) if filename else ''
-                        msg = 'Closed unpaired tag {} {}on line {}'.format(
-                            tag[1:], fn, line_no)
-                        raise ParseError(msg)
-                    self.handler.endElement(tag[1:])
-            else:
-                self.handler.line(line)
+                    self.handler.line(line)
+            except ParseError as pe:
+                # Re-raise
+                pe.args = (pe.args[0] + ' {}on line {}'.format(fn_msg, line_no),)
+                raise pe
+            except Exception as e:
+                raise ParseError('Error {}on line {}'.format(fn_msg, line_no)) from e
         if stack:
             raise ParseError('Stream ended with unclosed tags {}'.format(
                 '/'.join(stack)))
-        self.handler.endStream()
 
 
 class CorpusHandler:
@@ -137,43 +148,106 @@ class CorpusHandler:
         self.meta_fields = dict(kwargs)
 
         self.need_meta = self.meta or self.meta_fields
+        self.stack = set()
+        self.doc = None
+        self.skip_to_end = None
+        self.meta_data = None
+
+    def new_doc(self, attrs):
+        return Document(attrs, OrderedDict() if self.need_meta else None,
+                        [] if self.content else None)
 
     def startElement(self, tag, attrs):
-        pass
+        if not self.skip_to_end:
+            if tag in self.stack:
+                raise ParseError('Recursive tag declaration: <{}>'.format(tag))
+            if 'doc' not in self.stack:
+                if tag == 'doc':
+                    self.stack.add('doc')
+                    self.doc = self.new_doc(attrs if self.attrs else None)
+                else:
+                    raise ParseError('Unexpected tag <{}>'.format(tag))
+            else:
+                if tag == 'meta':
+                    if self.need_meta:
+                        self.stack.add('meta')
+                    else:
+                        self.skip_to_end = 'meta'
+                elif tag == 'p':
+                    self.stack.add('p')
+                    self.doc.paragraphs.append([])
+                elif 'meta' in self.stack:
+                    if self.meta_fields[tag] if tag in self.meta_fields else self.meta:
+                        # Meta field
+                        self.meta_data = []
+                else:
+                    raise ParseError('Unexpected tag <{}>'.format(tag))
 
     def endElement(self, tag):
-        pass
+        if self.skip_to_end:
+            if tag == self.skip_to_end:
+                self.skip_to_end = None
+            else:
+                return
+
+        self.stack.discard(tag)
+        if tag == 'doc':
+            self.queue.put(self.doc)
+            self.doc = None
+        elif tag == 'meta':
+            if not self.content:
+                self.skip_to_end = 'doc'
+        elif tag == 'p':
+            self.doc.paragraphs[-1] = '\n'.join(self.doc.paragraphs[-1])
+        elif 'meta' in self.stack:
+            # Finish any meta field
+            if self.meta_data is not None:
+                self.doc.meta[tag] = '\n'.join(self.meta_data)
+                self.meta_data = None
+        else:
+            raise ParseError('Unexpected tag </{}>'.format(tag))
 
     def line(self, line):
-        pass
+        if 'p' in self.stack:
+            self.doc.paragraphs[-1].append(line)
+        elif self.meta_data is not None:
+            self.meta_data.append(line)
 
-    def endStream(self):
-        pass
 
-
-def _parse(input, parse_fn, attrs=True, meta=True, content=True, **kwargs):
+def _parse(input, parse_fn, attrs=True, meta=True, content=True, **meta_fields):
     """Common background function for parse and parse_file."""
-    pass
+    queue = Queue()
+    ch = CorpusHandler(queue, attrs, meta, content, **meta_fields)
+    sp = SAXParser(ch, attrs)
+
+    with cf.ThreadPoolExecutor(max_workers=1) as executor:
+        future = executor.submit(parse_fn, sp, input)
+        while future.running():
+            try:
+                yield queue.get(timeout=0.1)
+            except Empty:
+                pass
+        return future.result()
 
 
-def parse(corpus_stream, attrs=True, meta=True, content=True, **kwargs):
+def parse(corpus_stream, attrs=True, meta=True, content=True, **meta_fields):
     """
     Enumerates Documents in a text stream in the corpus format. The rest of the
     parameters specify what parts of the documents to keep:
     - attrs: the attributes of the doc tag;
     - meta: the meta fields;
     - content: the textual content;
-    - kwargs: can be used to include / exclude specific meta fields. This
-              setting takes precedence over the generic meta argument.
+    - meta_fields: can be used to include / exclude specific meta fields. This
+                   setting takes precedence over the general meta argument.
     """
     yield from _parse(corpus_stream, SAXParser.parse,
-                      attrs, meta, content, **kwargs)
+                      attrs, meta, content, **meta_fields)
 
 
-def parse_file(corpus_file, attrs=True, meta=True, content=True, **kwargs):
+def parse_file(corpus_file, attrs=True, meta=True, content=True, **meta_fields):
     """
     Enumerates Documents in a text file in the corpus format. The arguments
     behave the same as in parse().
     """
     yield from _parse(corpus_file, SAXParser.parseFile,
-                      attrs, meta, content, **kwargs)
+                      attrs, meta, content, **meta_fields)
