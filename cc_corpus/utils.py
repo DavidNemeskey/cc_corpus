5b758fd6f0c68db2a0120c59b187af9e0a04c8cf
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-02-15 14:48:40 +0100

    Moved collect_inputs() into utils.py

--- cc_corpus/utils.py
+++ cc_corpus/utils.py
@@ -27,3 +27,20 @@ def openall(
     else:
         return open(filename, mode, buffering, encoding, errors, newline,
                     closefd, opener)
+
+
+def collect_inputs(inputs):
+    """
+    Collects all files from the files and directories specified.
+    """
+    # TODO: glob?
+    files = []
+    for input in inputs:
+        if op.isfile(input):
+            files.append(input)
+        elif op.isdir(input):
+            # TODO this doesn't work with a recursive directory structure
+            files.extend([op.join(input, f) for f in os.listdir(input)])
+        else:
+            raise ValueError('{} is neither a file nor a directory'.format(input))
+    return files
