af52c8893c0b8dd717f80dc029b2e8000a16b69d
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-02-21 13:03:30 +0100

    BatchWriter introduced.

--- scripts/minhash.py
+++ scripts/minhash.py
@@ -92,15 +92,21 @@ def process_file(input_file, permutations, n):
 
 class BatchWriter:
     """Writes batches of minhash data."""
-    def __init__(self, batch_size, zeroes=4):
+    def __init__(self, batch_size, out_dir, zeroes=4):
         self.batch_size = batch_size
+        self.out_dir = out_dir
         self.zeroes = zeroes
-        self.batch = 1
+        self.batch = 0
         self.minhashf = self.doc_idf == self.filef = None
         self.mh_offset = self.di_offset = 0
+        self.p_written = self.batch_size + 1  # so that we invoke new_file
+        self.total_written = 0
 
     def write_results(self, input_file, results):
         """Prints the results of minhashing a data file."""
+        if self.p_written >= self.batch_size:
+            self.new_file()
+
         print('{}\t{}\t{}\t{}'.format(input_file, len(results),
                                       self.mh_offset, self.di_offset),
               file=self.filef)
@@ -109,6 +115,38 @@ class BatchWriter:
         for doc, p in results['id']:
             self.di_offset += self.doc_idf.write(
                 '{}\t{}\n'.format(doc, p).encode('utf-8'))
+        self.p_written += len(results)
+
+    def new_file(self):
+        """Closes the old file and opens a new one."""
+        self.close()
+
+        self.batch += 1
+        prefix = os.path.join(self.out_dir,
+                              '{{:0{}}}'.format(self.zeroes).format(self.batch))
+        self.minhashf = open(prefix + '.minhashes', 'wb')
+        self.doc_idf = open(prefix + '.doc_ids', 'wb')
+        self.filef = open(prefix + '.files', 'wt')
+
+    def close(self):
+        """
+        Closes the currently written file handles. Called automatically when
+        the batch counter increases, but should also be called when processing
+        ends to close the files of the last batch.
+        """
+        if self.filef is not None:
+            self.minhashf.close()
+            self.doc_idf.close()
+            self.filef.close()
+            self.minhashf = self.doc_idf == self.filef = None
+
+            self.total_written += self.p_written
+            self.p_written = 0
+
+    def __del__(self):
+        """Just calls close()."""
+        super().__del__()
+        self.close()
 
 
 def main():
@@ -126,22 +164,18 @@ def main():
 
     files = sorted(collect_inputs(args.inputs))
     logging.info('Found a total of {} input files.'.format(len(files)))
+    writer = BatchWriter(args.batch_size, args.output_dir, args.zeroes)
     with Pool(args.processes) as pool:
         f = partial(process_file, permutations=args.permutations, n=args.n)
-        with openall('{}.minhashes'.format(args.output), 'wb') as mout, \
-             openall('{}.doc_ids'.format(args.output), 'wt') as dout:
-            num_ps = 0
-            for results in pool.map(f, files):
-                for mh in results['minhash']:
-                    mout.write(pickle.dumps(mh))
-                for doc, p in results['id']:
-                    print(doc, p, sep='\t', file=dout)
-                num_ps += len(results['minhash'])
-            logging.info('Hashed in total {} paragraphs.'.format(num_ps))
+        for results in pool.map(f, files):
+            writer.write_results(, results)
 
         pool.close()
         pool.join()
     logging.info('Done.')
+
+    writer.close()
+    logging.info('Hashed in total {} paragraphs.'.format(writer.total_written))
 
 
 if __name__ == '__main__':
