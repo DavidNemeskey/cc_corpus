f1dc278779fccf819e171ae975fd5acdbd3c329b
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-03-11 12:21:04 +0100

    lsh2.py -> lsh.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
         return f.read()
 
 setup(name='commoncrawl-downloader',
-      version='1.1.8',
+      version='1.1.9',
       description='A Python package for retrieving a list of urls and '
                   'specific files in bulk from Common Crawl, as well as '
                   'for processing the downloaded files.',
