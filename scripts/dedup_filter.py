0f5652934b1f10fd0593d2186d08938420ccfa9a
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-03-12 12:45:20 +0100

    dedup_filter.py finished.

--- scripts/dedup_filter.py
+++ scripts/dedup_filter.py
@@ -18,11 +18,10 @@ import logging
 from multiprocessing import Pool
 import os
 import os.path as op
-import re
-import shutil
-import sys
 
-from cc_corpus.deduplication import BatchWriter, read_batch
+from cc_corpus.corpus import parse_file
+from cc_corpus.deduplication import find_all_batches, read_batch
+from cc_corpus.utils import notempty, openall
 
 
 def parse_arguments():
@@ -42,7 +41,7 @@ def parse_arguments():
                              'the files have been moved.')
     parser.add_argument('--processes', '-P', type=int, default=1,
                         help='number of worker processes to use (max is the '
-                             'num of cores, default: 1).') 
+                             'num of cores, default: 1).')
     parser.add_argument('--log-level', '-L', type=str, default='info',
                         choices=['debug', 'info', 'warning', 'error', 'critical'],
                         help='the logging level.')
@@ -52,6 +51,34 @@ def parse_arguments():
         parser.error('Number of processes must be between 1 and {}'.format(
             num_procs))
     return args
+
+
+def deduplicate_batch_documents(batch_prefix, output_dir, input_dir=None):
+    """
+    Filters documents not present in the batch and writes the filtered corpus
+    files to output_dir. As above, input_dir can be specified if the location
+    information in the batch files is outdated.
+
+    Empty files will not be written.
+    """
+    batch_base = op.basename(batch_prefix)
+    logging.info('Filtering batch {}...'.format(batch_base))
+
+    kept, total = 0, 0
+    for input_file, results in read_batch(batch_prefix):
+        file_base = op.basename(input_file)
+        url_set = set(results['id'])
+        input_file = op.join(input_dir, file_base) if input_dir else input_file
+        with notempty(openall(op.join(output_dir, file_base), 'wt')) as outf:
+            for doc_no, doc in enumerate(parse_file(input_file), start=1):
+                if doc.attrs['url'] in url_set:
+                    print(doc, file=outf)
+                    kept += 1
+            total += doc_no
+
+    logging.info('Filtered batch {}; kept {} documents out of {}.'.format(
+        batch_base, kept, total))
+    return kept, total
 
 
 def main():
@@ -65,6 +92,22 @@ def main():
     os.nice(20)
     if not os.path.isdir(args.output_dir):
         os.makedirs(args.output_dir)
+
+    batch_prefixes = find_all_batches(args.input_dir)
+    logging.info('Found a total of {} batches.'.format(len(batch_prefixes)))
+
+    with Pool(args.processes) as pool:
+        f = partial(deduplicate_batch_documents,
+                    output_dir=args.output_dir, input_dir=args.input_dir)
+        kept, total = 0, 0
+        for batch_kept, batch_total in pool.imap(f, batch_prefixes):
+            kept += batch_kept
+            total += batch_total
+        pool.close()
+        pool.join()
+    logging.info('Done.')
+
+    logging.info('Kept {} documents out of {} in total'.format(kept, total))
 
 
 if __name__ == '__main__':
