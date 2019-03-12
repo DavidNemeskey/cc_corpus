2d070be046b4353ea47d597db85b73c955901510
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-03-12 12:22:02 +0100

    Added notempty() and NoEmptyWriteWrapper.

--- cc_corpus/utils.py
+++ cc_corpus/utils.py
@@ -44,6 +44,59 @@ def unpickle_stream(inf):
         return
 
 
+class NoEmptyWriteWrapper:
+    """
+    A file object wrapper deletes the file on close() if if was opened for
+    writing but nothing was written. Useful for file filtering tasks.
+
+    Truth be told, instead of this, a lazy file object (that only creates the
+    actual file on the first write()) would be much better, but it is much
+    more difficult to implement, due to the complexity of the io classes.
+    """
+    def __init__(self, f):
+        self._f = f
+        self._written = 0
+
+    def write(self, data):
+        written = self._f.write(data)
+        self._written += written
+        return written
+
+    def close(self):
+        self._f.close()
+        if self._written == 0:
+            if 'w' in self._f.mode:
+                os.remove(self._f.name)
+
+    def __enter__(self):
+        """
+        Enter needs to be defined, because for some reason, providing it through
+        __getattribute__ is not enough. Maybe because that returns the wrapped
+        object's __enter__; unfortunately, all we get is an AttributeError, so
+        it's hard to tell.
+        """
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """See __enter__."""
+        self.close()
+
+    def __getattribute__(self, name):
+        """
+        Returns the methods and data members defined in the object. Forwards
+        everything else to the wrapped file object.
+        """
+        if name in ['_f', '_written', '__enter__', '__exit__', 'write', 'close']:
+            return super().__getattribute__(name)
+        else:
+            return getattr(self._f, name)
+
+
+def notempty(f):
+    """Wraps f, a file object, in a NoEmptyWriteWrapper."""
+    return NoEmptyWriteWrapper(f)
+
+
 def collect_inputs(inputs):
     """
     Collects all files from the files and directories specified.
