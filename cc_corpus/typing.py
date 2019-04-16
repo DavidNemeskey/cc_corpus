0822ae5a25928e1a6acee17bf59fa09a6d56ae14
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-04-17 00:42:34 +0200

    Ooops...

--- cc_corpus/typing.py
+++ cc_corpus/typing.py
@@ -5,11 +5,11 @@
 Replacement for the typing library that only became available in 3.5.
 """
 
+__all__ = ['Any', 'BinaryIO', 'Dict', 'Iterator', 'List', 'Set', 'Tuple']
+
 # TODO: there should be a better way to do this. Maybe a corpus-level
 # __getattr__ or something?
 try:
     from typing import Any, BinaryIO, Dict, Iterator, List, Set, Tuple
 except ImportError:
-    Any, BinaryIO, Dict, Iterator, List, Set, Tuple = [None] * 6
-
-__all__ = ['Any', 'BinaryIO', 'Dict', 'Iterator', 'List', 'Set', 'Tuple']
+    Any, BinaryIO, Dict, Iterator, List, Set, Tuple = [None] * len(__all__)
