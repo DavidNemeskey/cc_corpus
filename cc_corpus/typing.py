8b2c0344f1cbd315628ed89b8e3537e1ecfa4161
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-05-13 00:13:03 +0200

    At least it works from other directories...

--- cc_corpus/typing.py
+++ cc_corpus/typing.py
@@ -8,14 +8,23 @@ we replace all types we need with a special class that supports []
 so that type declarations work.
 """
 
+from importlib import import_module
+import os
+import sys
+
 __all__ = ['Any', 'BinaryIO', 'Dict', 'Generator',
            'Iterator', 'List', 'Set', 'Tuple']
 
 # TODO: there should be a better way to do this. Maybe a corpus-level
 # __getattr__ or something?
 try:
-    from typing import Any, BinaryIO, Dict, Generator, Iterator, List, Set, Tuple
+    # Note that this doesn't work from the cc_corpus directory!
+    m = import_module('typing')
+    print(m)
+    for typ in __all__:
+        globals()[typ] = getattr(m, typ)
 except ImportError:
+    raise
     class GetItemType:
         def __getitem__(self, key):
             pass
