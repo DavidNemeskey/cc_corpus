81443b906a48600574f9edd81cf551ab36684b80
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-06-18 12:07:46 +0200

    Implemented renumber_corpus_files.py.

--- scripts/renumber_corpus_files.py
+++ scripts/renumber_corpus_files.py
@@ -7,27 +7,32 @@ used to even out the number of documents in the files.
 """
 
 from argparse import ArgumentParser
+from contextlib import closing
 import logging
-from multiprocessing import Pool
 import os
-import os.path as op
+import sys
 
-from multiprocessing_logging import install_mp_handler
-
-from cc_corpus.corpus import parse_file
+from cc_corpus.corpus import BatchWriter, parse_file
+from cc_corpus.utils import collect_inputs
 
 
 def parse_arguments():
     parser = ArgumentParser(__doc__)
-    parser.add_argument('--input-dir', '-i', required=True,
-                        help='the corpus directory')
+    parser.add_argument('input_dirs', nargs='+',
+                        help='the input directories.')
     parser.add_argument('--output-dir', '-o', required=True,
-                        help='the output directory')
-    parser.add_argument('--documents', '-d', required=True,
-                        help='the number of documents a file should contain.')
+                        help='the output directory.')
+    size_group = parser.add_mutually_exclusive_group()
+    size_group.add_argument('--documents', '-d', required=True,
+                            help='the number of documents a file should contain.')
+    size_group.add_argument('--keep-sizes', '-k', action='store_true',
+                            help='do not merge or split files; i.e. only '
+                                 'copies files to the output directory.')
+    parser.add_argument('--zeroes', '-Z', type=int, default=4,
+                        help='the number of zeroes in the output files\' names.')
     parser.add_argument('--processes', '-P', type=int, default=1,
                         help='number of worker processes to use (max is the '
-                             'num of cores, default: 1)')
+                             'num of cores, default: 1).')
     parser.add_argument('--log-level', '-L', type=str, default='info',
                         choices=['debug', 'info', 'warning', 'error', 'critical'],
                         help='the logging level.')
@@ -40,10 +45,6 @@ def parse_arguments():
     return args
 
 
-def read_files(input_file):
-    return [doc for doc in parse_file(input_file)]
-
-
 def main():
     args = parse_arguments()
 
@@ -51,22 +52,36 @@ def main():
         level=getattr(logging, args.log_level.upper()),
         format='%(asctime)s - %(process)s - %(levelname)s - %(message)s'
     )
-    install_mp_handler()
 
     os.nice(20)
     if not os.path.isdir(args.output_dir):
         os.makedirs(args.output_dir)
 
-    files = os.listdir(args.input_dir)
-    logging.info('Scheduled {} files for renumbering.'.format(len(files)))
+    input_files = collect_inputs(args.input_dirs)
+    logging.info('Scheduled {} files for renumbering.'.format(len(input_files)))
 
-    with Pool(args.processes) as pool:
-        pool.close()
-        pool.join()
+    batch_size = args.documents if not args.keep_sizes else sys.maxsize
+    num_docs = 0
+    with closing(BatchWriter(batch_size, args.output_dir, args.zeroes)) as bw:
+        for input_file in input_files:
+            if not args.keep_sizes:
+                logging.debug('Reading file {}...'.format(input_file))
+                for document in parse_file(input_file):
+                    bw.write(document)
+                    num_docs += 1
+            else:
+                logging.debug('Copying file {}...'.format(input_file))
+                bw.copy_file(input_file)
 
-    logging.info('Done. Renumbered {} files to {}, {} documents each; '
-                 '{} documents in total.'.format(
-                     len(files), XXX, args.documents, YYY))
+    if not args.keep_sizes:
+        logging.info(
+            'Done. Renumbered {} files to {}, {} documents each; '
+            '{} documents in total.'.format(
+                len(input_files), args.output_dir, args.documents, num_docs)
+        )
+    else:
+        logging.info('Done. Renumbered {} files to {}.'.format(
+                     len(input_files), args.output_dir))
 
 
 if __name__ == '__main__':
