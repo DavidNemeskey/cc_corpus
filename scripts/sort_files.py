673c637912ed553b3613f6b2347bbc2b6f76736e
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-07-22 11:27:09 +0200

    Renamed the sorting scripts.

--- scripts/sort_files.py
+++ scripts/sort_files.py
@@ -1,53 +1,65 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
 """
-Sorts all entries in a list of index files.
+Sorts corpus files based on the THE FIRST URL in them. In other words, this
+script can be used to re-sort the corpus by domain after the exact order has
+been disrupted by concurrent processing by the last step of
+frequent_paragraphs.py.
 
-I am not sure how much the parallellism helps, as there is a "reduce" step
-at the end. Also, it would probably be faster to use the fact that the files
-are sorted after sort_one_file() and do a merge-sort.
+This script renames the files in the input directory.
 """
 
 from argparse import ArgumentParser
-import concurrent.futures as cf
-import gzip
-from itertools import chain
+import logging
+from operator import itemgetter
 import os
-import os.path as op
+import re
+import shutil
+
+from cc_corpus.utils import openall
 
 
 def parse_arguments():
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('input_dir', help='the index directory')
-    parser.add_argument('--processes', '-p', type=int, default=1,
-                        help='number of worker processes to use (max is the '
-                             'num of cores, default: 1)')
-    args = parser.parse_args()
-    num_procs = len(os.sched_getaffinity(0))
-    if args.processes < 1 or args.processes > num_procs:
-        parser.error('Number of processes must be between 1 and {}'.format(
-            num_procs))
-    return args
-
-
-def sort_one_file(file_name):
-    with gzip.open(file_name, 'rt') as inf:
-        # After filtering, the line is prepended with the "domain"
-        # I skip that and extract it myself; hence the [:7][-6:] part
-        to_sort = [(segment, int(offset), int(length))
-                   for segment, offset, length in
-                   (line.split()[:7][-6:][1:4] for line in map(str.strip, inf))]
-        to_sort.sort()
-        return to_sort
+    parser.add_argument('--input-dir', '-i', required=True,
+                        help='the input directory.')
+    parser.add_argument('--log-level', '-L', type=str, default='info',
+                        choices=['debug', 'info', 'warning', 'error', 'critical'],
+                        help='the logging level.')
+    return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    to_process = [op.join(args.input_dir, f) for f in os.listdir(args.input_dir)]
-    with cf.ProcessPoolExecutor(max_workers=args.processes) as executor:
-        for segment, offset, length in sorted(chain(*executor.map(sort_one_file, to_process))):
-            print('{}\t{}\t{}'.format(segment, offset, length))
+
+    logging.basicConfig(
+        level=getattr(logging, args.log_level.upper()),
+        format='%(asctime)s - %(process)s - %(levelname)s - %(message)s'
+    )
+    os.nice(20)
+
+    os.chdir(args.input_dir)
+    input_files = sorted(os.listdir('.'))
+    logging.info('Found a total of {} input files.'.format(len(input_files)))
+
+    url_p = re.compile(r'^<doc .*? url="([^"]+)" .*>$')
+    first_urls = []
+    for f in input_files:
+        with openall(f) as inf:
+            header = inf.readline()
+            url = url_p.match(header).group(1)
+            first_urls.append((url, f))
+    first_urls.sort()
+
+    for old_doc, new_doc in zip(map(itemgetter(1), first_urls),
+                                sorted(input_files)):
+        shutil.move(old_doc, 'new_' + new_doc)
+
+    for f in os.listdir('.'):
+        shutil.move(f, f[4:])
+
+    logging.info('Done.')
 
 
 if __name__ == '__main__':
