e32b66d8b3761074c4c689c3bdaa1137f105d29c
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2022-10-06 15:17:50 +0000

    Dropping support for Python 3.6, versions supported are 3.7--3.10.

--- setup.py
+++ setup.py
@@ -43,8 +43,10 @@ setup(name='commoncrawl-downloader',
 
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7'
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
       ],
       keywords='corpus common crawl hungarian',
       packages=find_packages(exclude=['scripts']),
