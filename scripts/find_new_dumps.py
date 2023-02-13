56975a934552d0162c33d6736cd87ba73d945fb3
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2023-02-13 11:33:02 +0000

    A few fixes + documentation.

--- scripts/find_new_dumps.py
+++ scripts/find_new_dumps.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
-Find and collect new indexes from common_crawl
+Finds and collects new (not yet downloaded) indices from common_crawla. The
+new indices are then printed to the screen, or are sent to a REST endpoint.
 """
 
 import bs4
@@ -11,20 +15,15 @@ from typing import Optional
 import warnings
 
 
-warnings.filterwarnings("ignore")
-
-
 def parse_arguments():
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('--input_dir', '-d', type=Path, help='Directory where previous info is stored.')
+    parser.add_argument('--index_dir', '-d', type=Path, help='Directory where previous info is stored.')
     parser.add_argument('--url', '-u', type=str, help='REST API url')
     args = parser.parse_args()
     return args
 
 
-"""
-Pairing months by number.
-"""
+# Mapping months to the corresponding two-digit numbers.
 months_dict = {
     'January': '01',
     'February': '02',
@@ -46,9 +45,7 @@ months_dict = {
 
 
 def get_info_from_line(li: bs4.element.Tag) -> Optional[tuple]:
-    """
-    Specify 'CC-MAIN' structure
-    """
+    """Parses the 'CC-MAIN' list item for the link."""
     if 'CC-MAIN' in li.text:
         month_year_str = li.find('a').text
         month_str, year_str = month_year_str.split(' ')
@@ -62,12 +59,23 @@ def get_info_from_line(li: bs4.element.Tag) -> Optional[tuple]:
         return None
 
 
-def get_info_dict(input_dir: Optional[Path]) -> dict:  # Optinal[path]
+def get_info_dict(index_dir: Optional[Path]) -> dict[str, str]:
     """
-    Retrieve 'CC-MAIN' from the website
+    Retrieves the monthly links to the 'CC-MAIN' index from the _Get started_
+    page of the Common Crawl website. Only the indices not downloaded yet are
+    returned.
+
+    :param index_dir: a directory that contains the already downloaded monthly
+                      indices. If specified, the indices corresponding to
+                      its subdirectories (those that conform to the naming
+                      scheme described below) will be excluded from the
+                      returned dictionary.
+    :return: a directory of the (new) monthly indices, as a mapping from
+             the readily understandable ``yyyy_mm`` date format to their names
+             in the Common Crawl.
     """
-    if input_dir is not None:
-        ind_in_dir = {path.name for path in input_dir.iterdir()}
+    if index_dir is not None:
+        ind_in_dir = {path.name for path in index_dir.iterdir()}
     else:
         ind_in_dir = set()
     webpage = requests.get("https://commoncrawl.org/the-data/get-started/")
@@ -84,29 +92,38 @@ def get_info_dict(input_dir: Optional[Path]) -> dict:  # Optinal[path]
                             date_str, index_name = info
                             if date_str not in ind_in_dir:
                                 out_dict[date_str] = index_name
-
                     break
 
     return out_dict
 
 
-def help_func(info_dict):
-
+def print_dict_to_console(info_dict: dict[str, str]):
+    """
+    Prints the dictionary returned by :func:`get_info_dict` to console. Used
+    when no REST API URL is specified on the command line.
+    """
     for key, value in info_dict.items():
         print(key, value, sep='\t')
 
 
-def send_dict_to_url(url: str, info_dict: dict) -> None:
+def send_dict_to_url(url: str, info_dict: dict[str, str]):
+    """
+    Sends the index dictionary returned by :func:`get_info_dict` to a REST
+    endpoint.
+    """
     r = requests.post(url, data=json.dumps(info_dict))
 
 
 def main():
+    # Get rid of BeautifulSoup warnings
+    warnings.filterwarnings("ignore")
+
     args = parse_arguments()
-    info_dict = get_info_dict(args.input_dir)
+    info_dict = get_info_dict(args.index_dir)
     if args.url:
         send_dict_to_url(args.url, info_dict)
     else:
-        help_func(info_dict)
+        print_dict_to_console(info_dict)
 
 
 if __name__ == '__main__':
