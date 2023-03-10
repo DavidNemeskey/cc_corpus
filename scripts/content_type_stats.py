8a90c1b9a7f39c1e6ff9c7854c713bf902796cb7
acheronw <szekeresam@gmail.com>
2023-03-10 14:33:05 +0100

    Reorganized pooling to be on a per input file instead of a per input batch basis

--- scripts/content_type_stats.py
+++ scripts/content_type_stats.py
@@ -14,7 +14,6 @@ Writes the output as a tsv.gz.
 
 
 from argparse import ArgumentParser
-from functools import partial
 import logging
 from multiprocessing import Pool
 from pathlib import Path
@@ -28,12 +27,9 @@ def parse_arguments():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--input-dir', '-i', type=Path, required=True,
                         help='the input directory that contains the corpus')
-    parser.add_argument('--output-dir', '-o', type=Path, required=True,
-                        help='the directory where results are written')
-    parser.add_argument('--from-dir', '-f',
-                        help='the earlies batch to work on.')
-    parser.add_argument('--upto-dir', '-u',
-                        help='the latest batch to work on.')
+    parser.add_argument('--output-file', '-o', type=Path, required=True,
+                        help='the file where results are written. It should be'
+                             'a .tsv.gz file')
     parser.add_argument('--processes', '-P', type=int, default=1,
                         help='number of worker processes to use (max is the '
                              'num of cores, default: 1).')
@@ -45,75 +41,51 @@ def parse_arguments():
     return args
 
 
-def fetch_directories(root_dir: Path, from_dir: Path, upto_dir: Path
-                      ) -> [Path]:
+def process_file(input_file: Path) -> str:
     """
-    Collects the batches within the rootdir, that are not earlier than
-    from_dir and not later than upto_dir.
+    Processes a file, collecting mime-type data from the headers of the docs
+    contained in that file.
+    It returns a string which is a tsv: each document is a single line and the
+    values are separated by tabs within the line.
     """
-    list_of_dirs = [dir for dir in sorted(root_dir.iterdir())]
-    if upto_dir:
-        try:
-            upto_i = [dir.name for dir in list_of_dirs].index(upto_dir)
-        except ValueError:
-            raise ValueError('The upto-dir must be a valid directory')
-        list_of_dirs = list_of_dirs[: upto_i + 1]
-    if from_dir:
-        try:
-            from_i = [dir.name for dir in list_of_dirs].index(from_dir)
-        except ValueError:
-            raise ValueError('The from-dir is not a valid batch')
-        list_of_dirs = list_of_dirs[from_i:]
-    return list_of_dirs
-
-
-def process_batch(input_dir, output_dir):
-    """
-    Processes a batch, collecting mime-type data from its files.
-    Writes the results into a single tsv.gz file.
-    """
-    stat_file = (output_dir / input_dir.name).with_suffix('.tsv.gz')
+    results = ""
     matcher_ct = re.compile(r'Content-Type: ([\w/]+)', re.I)
     matcher_wi = re.compile(r'WARC-IDentified-Payload-Type: ([\w/]+)', re.I)
     matcher_cd = re.compile(r'(Content-Disposition: )([^\n^"]*)"([^\n^"]*)"', re.I)
-    with openall(stat_file, "wt", encoding="utf-8") as f:
-        for file in input_dir.iterdir():
-            file_name = input_dir.name + '/' + file.name
-            for doc in parse_file(file):
-                # Get the type from the metadata of the doc tag:
-                if doc.attrs['mime-type']:
-                    type_from_doctag = doc.attrs['mime-type']
-                else:
-                    type_from_doctag = '-'
-                # Get the type from the warc request header:
-                match_warcid = matcher_wi.search(doc.meta['request'])
-                if match_warcid:
-                    type_from_warc_id = match_warcid.groups(1)[0]
-                else:
-                    type_from_warc_id = '-'
-                # Get the type from the response header:
-                match_resp = matcher_ct.search(doc.meta['response'])
-                if match_resp:
-                    type_from_response = match_resp.groups(1)[0]
-                else:
-                    type_from_response = '-'
-                # Get the info about the attached file, if any:
-                match_cd = matcher_cd.search(doc.meta['response'])
-                if match_cd:
-                    attached_file = match_cd.group(3)
-                    if '.' in attached_file:
-                        attachment_ext = attached_file.split('.')[-1]
-                    else:
-                        attachment_ext = 'no_extension'
-                else:
-                    attachment_ext = '-'
-                # Process the results for this doc:
-                line = file_name + '\t'
-                line += type_from_doctag + '\t'
-                line += type_from_warc_id + '\t'
-                line += type_from_response + '\t'
-                line += attachment_ext + '\t'
-                print(line, file=f)
+    for doc in parse_file(input_file):
+        # Get the type from the metadata of the doc tag:
+        if doc.attrs['mime-type']:
+            type_from_doctag = doc.attrs['mime-type']
+        else:
+            type_from_doctag = '-'
+        # Get the type from the warc request header:
+        match_warcid = matcher_wi.search(doc.meta['request'])
+        if match_warcid:
+            type_from_warc_id = match_warcid.groups(1)[0]
+        else:
+            type_from_warc_id = '-'
+        # Get the type from the response header:
+        match_resp = matcher_ct.search(doc.meta['response'])
+        if match_resp:
+            type_from_response = match_resp.groups(1)[0]
+        else:
+            type_from_response = '-'
+        # Get the info about the attached file, if any:
+        match_cd = matcher_cd.search(doc.meta['response'])
+        if match_cd:
+            attached_file = match_cd.group(3)
+            if '.' in attached_file:
+                attachment_ext = attached_file.split('.')[-1]
+            else:
+                attachment_ext = 'no_extension'
+        else:
+            attachment_ext = '-'
+        # Process the results for this doc:
+        line = '\t'.join((str(input_file), type_from_doctag,
+                          type_from_warc_id, type_from_response,
+                          attachment_ext,))
+        results += line + '\n'
+    return results
 
 
 def main():
@@ -124,14 +96,13 @@ def main():
         format='%(asctime)s - %(process)s - %(levelname)s - %(message)s'
     )
 
-    args.output_dir.mkdir(parents=True, exist_ok=True)
-    task_dirs = fetch_directories(args.input_dir, args.from_dir, args.upto_dir)
-    logging.debug(f'The directories to process are: {task_dirs}')
-
-    f = partial(process_batch, output_dir=args.output_dir)
-    with Pool(args.processes) as p:
-        for _ in p.imap_unordered(f, task_dirs):
-            pass
+    args.output_file.parent.mkdir(parents=True, exist_ok=True)
+    with openall(args.output_file, "wt", encoding="utf-8") as f:
+        with Pool(args.processes) as p:
+            for stats in p.imap_unordered(process_file, args.input_dir.iterdir()):
+                print(stats, file=f)
+        p.close()
+        p.join()
 
 
 if __name__ == '__main__':
