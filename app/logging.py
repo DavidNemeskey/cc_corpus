10ecc2adae8297f05a4fc3baab3d4c3bc4fcc743
acheronw <szekeresam@gmail.com>
2024-02-02 11:26:06 +0100

    cosmetic changes

--- app/logging.py
+++ app/logging.py
@@ -2,11 +2,11 @@ import logging
 
 from .config import config, get_logs_dir
 
+
 def configure_logging():
     logging.basicConfig(
-        filename= get_logs_dir(config) / "server.log",
+        filename=get_logs_dir(config) / "server.log",
         filemode='a',
         level=logging.INFO,
         format='%(asctime)s - %(threadName)-10s)- %(levelname)s - %(message)s'
     )
-
