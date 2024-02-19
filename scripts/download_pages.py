f47e905e45719cdc1b34a9bbb611cb94441b0b7e
acheronw <szekeresam@gmail.com>
2024-02-19 10:07:15 +0100

    Removed old code and unused imports from download_pages.py

--- scripts/download_pages.py
+++ scripts/download_pages.py
@@ -19,17 +19,12 @@ time.
 """
 import queue
 from argparse import ArgumentParser
-from concurrent.futures import ThreadPoolExecutor
 import boto3
 import gzip
 import hashlib
-from itertools import groupby
 import logging
-from operator import itemgetter
 import os
 from pathlib import Path
-from queue import Empty, Full, Queue
-import signal
 import subprocess
 from tempfile import gettempdir
 import threading
@@ -285,8 +280,8 @@ def download_collected_ranges(ranges_dir: Path,
                     outf, doc_file = open_files(tid, chunk)
                 progress_bar.update(1)
             else:
-                # TODO What should we do?
-                # The errors are already logged in download_document.
+                # The errors are already logged by the download_document()
+                # method.
                 pass
             q.task_done()
         outf.close()
@@ -316,132 +311,6 @@ def download_collected_ranges(ranges_dir: Path,
     errf.close()
     progress_bar.close()
     logging.info('Download completed.')
-
-    # This was the previous version:
-    #
-    # q = Queue(num_threads * 2)
-    #
-    # # Signal handling so that the script can be interrupted / terminated
-    # # gracefully. See https://stackoverflow.com/questions/65832061/
-    # exiting = threading.Event()
-    # error_lock = threading.Lock()
-    # def signal_handler(signum, frame):  # noqa
-    #     print('Stopping after all ongoing downloads have completed. This '
-    #           'may take some time...')
-    #     logging.warning(f'Received signal {signum}. Exiting...')
-    #     exiting.set()
-    # signal.signal(signal.SIGINT, signal_handler)
-    # signal.signal(signal.SIGTERM, signal_handler)
-    #
-    #
-    #
-    # # TODO use a condition to signal end of processing
-    # def producer():
-    #     """Groups ranges in the index by warc and puts them into the queue."""
-    #     logging.info('Producer running')
-    #     with openall(ranges_file, 'rt') as inf:
-    #         it = (line.strip().split() for line in inf)
-    #         for warc, ranges in groupby(it, key=itemgetter(2)):
-    #             ranges_list = list(ranges)
-    #             # logging.info(f'Adding {warc=} ({len(l)}) {l=}...')
-    #             while not exiting.is_set():
-    #                 try:
-    #                     q.put((warc, ranges_list), True, 5)
-    #                     break
-    #                 except Full:
-    #                     pass
-    #     # To signal the end of processing
-    #     logging.info('Producer ended!')
-    #     if not exiting.is_set():
-    #         q.put((None, (None, None)))
-    #
-    # def consumer(tid: int, progress_bar, errf: TextIO):
-    #     """
-    #     Downloads the byte ranges read from the queue and writes them to disk.
-    #     """
-    #     logging.info(f'Consumer {tid} started....')
-    #     chunk, written = 1, 0
-    #
-    #     def open_files():
-    #         """Opens a new output index and document file."""
-    #         file_name = f'{file_prefix}_{tid}_{file_padding.format(chunk)}.gz'
-    #         return (
-    #             notempty(openall(index_out_dir / f'{file_name}', 'wt')),
-    #             RotatedGzip(str(data_out_dir), chunk_size,
-    #                         os.path.splitext(file_name)[0], doc_padding,
-    #                         extension)
-    #         )
-    #
-    #     outf, doc_file = open_files()
-    #     try:
-    #         while not exiting.is_set():
-    #             try:
-    #                 warc, index_lines = q.get(True, 5)
-    #             except Empty:
-    #                 continue
-    #             if warc is not None:
-    #                 ranges = [(int(index[3]), int(index[4]))
-    #                           for index in index_lines]
-    #                 try:
-    #                     st = time.time()
-    #                     downloaded = download_warc_ranges(warc, ranges, retries)
-    #                     logging.info(f'Downloaded in {time.time() - st:.2f} seconds.')
-    #                     for index, doc in zip(index_lines, downloaded):
-    #                         if doc is None:
-    #                             continue
-    #                         index_str = ' '.join(index)
-    #                         try:
-    #                             decompressed = zlib.decompress(doc, zlib.MAX_WBITS | 32)
-    #                             print(index_str, file=outf)
-    #                             doc_file.write(decompressed)
-    #                             written += 1
-    #                             if written == lines_per_file:
-    #                                 outf.close()
-    #                                 doc_file.close()
-    #                                 chunk, written = chunk + 1, 0
-    #                                 outf, doc_file = open_files()
-    #                         except zlib.error:
-    #                             logging.exception(
-    #                                 'Decompression error occured for '
-    #                                 f'`{index_str}.`'
-    #                             )
-    #                 except DownloadError as de:
-    #                     logging.error(f'Could not download {warc}: {de}.')
-    #                     error_lock.acquire()
-    #                     for index in index_lines:
-    #                         print(' '.join(index), file=errf)
-    #                     error_lock.release()
-    #                 progress_bar.update(1)
-    #             else:
-    #                 # Put the item signalling end of processing back so that
-    #                 # other threads get it as well.
-    #                 q.put((warc, index_lines))
-    #                 break
-    #     except:  # noqa
-    #         logging.exception(f'Exception in {tid}: exiting...')
-    #         exiting.set()
-    #     finally:
-    #         logging.info(f'Consumer {tid} finished, written {chunk} files.')
-    #         outf.close()
-    #         doc_file.close()
-    #
-    # index_out_dir.mkdir(parents=True, exist_ok=True)
-    # data_out_dir.mkdir(parents=True, exist_ok=True)
-    # error_file.parent.mkdir(parents=True, exist_ok=True)
-    #
-    # thread_padding = f'{{:0{num_digits(num_threads)}}}'
-    # progress_bar = otqdm(desc='Downloading WARC ranges...')
-    # errf = openall(error_file, 'wt')
-    # try:
-    #     with ThreadPoolExecutor(max_workers=num_threads + 1) as executor:
-    #         executor.submit(producer)
-    #         for tid in range(1, num_threads + 1):
-    #             executor.submit(consumer,
-    #                             thread_padding.format(tid), progress_bar, errf)
-    #     logging.info('Download completed.')
-    # finally:
-    #     errf.close()
-    #     progress_bar.close()
 
 
 def main():
